9ddf7ef93b00f447728ef477aa087bf58d980b22
SmileyShield <85408313+SmileyShield@users.noreply.github.com>
2021-06-06 15:37:50 +0100

    Create RegisterHandler.py

---
+++ RegisterHandler.py
@@ -0,0 +1,32 @@
+import os
+import getpass as gp
+from colorama import Fore, Style
+from connector import mycursor
+from time import sleep
+
+class userRegister:
+      def __init__(self):
+            pass
+
+      def registerScreen(self):
+            print(Fore.CYAN + '''
+                  +----------------------------+
+                  |          Register          |
+                  +----------------------------+
+            ''' + Style.RESET_ALL
+            )
+
+            self.email = str(input('E-mail: ' + Fore.GREEN))
+            print(Style.RESET_ALL)
+            self.name = str(input('Full Name: '))
+            self.password = gp.getpass(prompt='Password' + Fore.WHITE + Style.DIM + '[hidden]' + Style.RESET_ALL + ': ', stream=None)
+
+      def doRegister(self):
+            try:
+                  mycursor.execute(f'INSERT INTO users(email, name, password) VALUES ("{self.email}", "{self.name}", "{self.password}")')
+                  os.environ['AUCUSER'], os.environ['AUCMAIL'] = self.name, self.email
+            except:
+                  print(Fore.RED + 'The email exists already or something went wrong! Redirecting back to main menu.' + Style.RESET_ALL)
+                  sleep(5)
+
+            return
